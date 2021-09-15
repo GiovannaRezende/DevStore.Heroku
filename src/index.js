10ac39d85e35d7a7b7bb05ae3883acd9079e8308
GiovannaRezende <giovannadossantos2004@gmail.com>
2021-09-15 00:03:10 -0300

    ajudai

--- src/index.js
+++ src/index.js
@@ -17,12 +17,12 @@ app.get('/produtos', async (req, resp) => {
 
 app.post('/produtos', async (req, resp) => {
     try {
-        let { produto, categoria, precode, precopor, avaliacao, descricao, estoque, imagem, ativo, inclusao } = req.body;
+        let { produto, categoria, precode, precopor, avaliacao, descricao, estoque, imagem } = req.body;
 
         let m = await db.tb_produto.findOne({ where: { nm_produto: produto,
              ds_categoria: categoria, vl_preco_de: precode, vl_preco_por: precopor,
-              vl_avaliacao:avaliacao, ds_produto: descricao, qtd_estoque: estoque,
-               img_produto: imagem, bt_ativo: ativo, dt_inclusao: inclusao }});
+              vl_avaliacao: avaliacao, ds_produto: descricao, qtd_estoque: estoque,
+               img_produto: imagem }});
 
         if (m != null)
             return resp.send({ erro: 'Produto já existe!' });
@@ -36,8 +36,6 @@ app.post('/produtos', async (req, resp) => {
             ds_produto: descricao,
             qtd_estoque: estoque,
             img_produto: imagem,
-            bt_ativo: ativo,
-            dt_inclusao: inclusao
         })
         resp.send(r);
     } catch (e) {
@@ -51,7 +49,7 @@ app.put('/produtos/:id', async (req, resp) => {
         let id = req.params.id;
         let p = req.body;
         
-        let r = await db.tb_matricula.update(
+        let r = await db.tb_produto.update(
             {
                 nm_produto: p.produto,
                 ds_categoria: p.categoria,
@@ -61,8 +59,6 @@ app.put('/produtos/:id', async (req, resp) => {
                 ds_produto: p.descricao,
                 qtd_estoque: p.estoque,
                 img_produto: p.imagem,
-                bt_ativo: p.ativo,
-                dt_inclusao: p.inclusao
             },
             {
                 where: { id_produto: id }
